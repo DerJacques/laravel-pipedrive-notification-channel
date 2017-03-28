c38ce7b0742cef4ccdba2b81d9389dfbd6e0e28a
Manuel Thomsen <manuel@manuelt.de>
2017-03-28 11:16:19 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Exceptions/FailedRequest.php
+++ src/Exceptions/FailedRequest.php
@@ -6,11 +6,11 @@ class FailedRequest extends \Exception
 {
     public static function rejected($errorCode)
     {
-        return new static('Pipedrive rejected the request with error code '. $errorCode);
+        return new static('Pipedrive rejected the request with error code '.$errorCode);
     }
 
     public static function missingRequiredAttribute($attribute)
     {
-        return new static('Missing required attribute: '. $attribute);
+        return new static('Missing required attribute: '.$attribute);
     }
 }
